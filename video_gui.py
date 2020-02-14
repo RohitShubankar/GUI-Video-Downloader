eeff60ae3e7f02454824aeaaaf4afa8ea334c7d8
Shubankar <rohit.shubankar@gmail.com>
2020-02-15 03:51:38 +0530

    Update video_gui.py

--- video_gui.py
+++ video_gui.py
@@ -6,8 +6,25 @@ A simple python based gui application to download videos from youtube.
 import tkinter as tk
 import os
 import youtube_dl
+import tkinter.font as tkfont
 from tkinter import *
 from tkinter.ttk import *
+
+root = tk.Tk()
+pic = PhotoImage(file ='C://Users//rohit//Documents//GitHub//GUI-Video-Downloader//index.jpg')
+root.iconphoto(False,pic)
+root.title('Video Downloader')
+root.geometry('400x400')
+root.resizable(width=FALSE,height=FALSE)
+
+canvas1 = tk.Canvas(root,height = 400, width=400, bg="#263d42")
+canvas1.pack()
+bold_font = tkfont.Font(family="Helvetica",size=12,weight="bold")
+label1 = tk.Label(root,text= "Enter the URL",width=10,bg="#263d42")
+label1.config(font=bold_font)
+canvas1.create_window(200,100,window=label1)
+download_entry = tk.Entry(root)
+canvas1.create_window(200,140,window=download_entry)
 
 def get_video_url():
    search_item = download_entry.get()
@@ -19,15 +36,11 @@ def get_video_url():
    os.chdir('C:/Users/rohit/Desktop/download')
    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download([search_item])
+   bold_font2 = tkfont.Font(family="Helvetica",size=10,weight="bold")
+   label2 =tk.Label(root,text="Video Downloaded",width=20,bg="#263d42")
+   label2.config(font=bold_font2)
+   canvas1.create_window(200,300,window=label2)
 
-root = tk.Tk()
-pic = PhotoImage(file ='C://Users//rohit//Documents//GitHub//GUI-Video-Downloader//index.jpg')
-root.iconphoto(False,pic)
-root.title('Video Downloader')
-canvas1 = tk.Canvas(root,height = 400, width=400, bg="#263d42")
-canvas1.pack()
-download_entry = tk.Entry(root)
-canvas1.create_window(200,140,window=download_entry)
 download = tk.Button(text= "Download", padx=5,pady=5,fg = "white",bg = "DeepSkyBlue4",command = get_video_url)
 canvas1.create_window(200,230,window=download)
 root.mainloop()
